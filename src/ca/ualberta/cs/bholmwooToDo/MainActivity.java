dcf1a077b6814a14f2824081a967609791e370a6
BHolmwood <bholmwoo@ualberta.ca>
2014-09-17 12:53:10 -0600

    Testing some stuff

--- src/ca/ualberta/cs/bholmwooToDo/MainActivity.java
+++ src/ca/ualberta/cs/bholmwooToDo/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 		@Override
 		public int getCount() {
 			// Show 3 total pages.
-			return 3;
+			return 2;
 		}
 
 		@Override
