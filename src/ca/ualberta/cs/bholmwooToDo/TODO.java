37f9cc9bf371bfbe41a806b426622c75428cc258
BHolmwood <bholmwoo@ualberta.ca>
2014-09-18 23:18:52 -0600

    Added TODO and TODOList classes.

---
+++ src/ca/ualberta/cs/bholmwooToDo/TODO.java
@@ -0,0 +1,33 @@
+package ca.ualberta.cs.bholmwooToDo;
+
+public class TODO {
+
+	private String text;
+	private Boolean status;
+	
+	
+	//Constructor
+	//Set TODO text to input text and set default status to false (not done)
+	public TODO(String text) {
+		super();
+		this.text = text;
+		this.status = false;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public Boolean getStatus() {
+		return status;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+}
---
+++ src/ca/ualberta/cs/bholmwooToDo/TODOList.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cs.bholmwooToDo;
+
+import java.util.ArrayList;
+
+public class TODOList extends ArrayList {
+
+	public TODOList() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
